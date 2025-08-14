0f3b61fa02d22610afffe37d563262e0fce8498d
Charls Rebaja <charlsrebaja09@gmail.com>
2025-08-14 12:31:07 +0800

    Describe your changes here

--- src/app/lessons/[lessonId]/page.tsx
+++ src/app/lessons/[lessonId]/page.tsx
@@ -2,11 +2,12 @@ import { lessons } from "@/data/lessons";
 import { notFound } from "next/navigation";
 import LessonContent from "./LessonContent";
 
-export default async function LessonPage({
-  params,
-}: {
+interface PageProps {
   params: { lessonId: string };
-}) {
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+export default async function LessonPage({ params }: PageProps) {
   const lesson = lessons.find((l) => l.id === params.lessonId);
 
   if (!lesson) {
